583339eed754b54dcb627dd3cc185a17e3e6031f
Zeb Sola <zebsola@gmail.com>
2023-06-07 20:57:48 -0700

    fix api again lol

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const getPosts = async () => {
-    const response = await axios.get('/api/posts', {
+    const response = await axios.get('https://strapi-3nhz.onrender.com/api/posts', {
         headers: {
-            Authorization: 'bearer ' + import.meta.env.PROD_API_SECRET
+            Authorization: 'bearer ' + import.meta.env.VITE_API_SECRET
         }
     });
     return response.data.data;
-    // console.log(response)
+
 };
 
 export default getPosts;
